2fdcdba30ab0848d4c9d4c0bccf8ca95798e7b37
hanameee <hanameee@gmail.com>
2022-10-18 01:48:02 +0900

    Fix: webpack 관련 dependency 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,10 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 const path = require("path");
 
+// 단순한 프로젝트이므로 오직 js 번들링만 진행. HTML/CSS는 웹팩 없이 dist 폴더에서 관리
 module.exports = {
     entry: "./src/main.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [new MiniCssExtractPlugin()],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-        ],
-    },
+    }
 };
